3a09a536eb8b43f290b90005f4ebf4270de7240c
pupil <pupil@ivkhk.ee>
2021-10-01 11:19:49 +0300

    author, book, reader

---
+++ src/myclasses/Author.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myclasses;
+
+/**
+ *
+ * @author pupil
+ */
+public class Author {
+    private String name;
+    private String lastname;
+    private int year;
+    private int day;
+    private int month;
+    
+    public Author() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+    
+}
---
+++ src/myclasses/Book.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myclasses;
+
+/**
+ *
+ * @author pupil
+ */
+public class Book {
+    private String caption;
+    private Author[] author;
+    private int publishedYear;
+    public Book() {
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public Author[] getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author[] author) {
+        this.author = author;
+    }
+
+    public int getPublishedYear() {
+        return publishedYear;
+    }
+
+    public void setPublishedYear(int publishedYear) {
+        this.publishedYear = publishedYear;
+    }
+    
+    
+}
